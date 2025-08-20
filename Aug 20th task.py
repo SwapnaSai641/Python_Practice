9fce17f2e77e19445202a4de76baf86998b90f64
SwapnaSai641 <saiswapna335@gmail.com>
2025-08-20 22:20:19 +0530

    Add files via upload

---
+++ Aug 20th task.py
@@ -0,0 +1,54 @@
+#menu question
+# while(True):
+#     print('1.Square of a number  2.Cube of a number  3.Addition  4.Subtraction  5.Multiplication  6.Division  7.Exit'  )
+#     choice = input('Enter your choice')
+#     if choice=='1' or choice=='square':
+#         num=int(input('Enter a number for squaring'))
+#         print(num**2)
+#     elif choice == '2' or choice=='cube':
+#         num=int(input('Enter a number to cude'))
+#         print(num**3)
+#     elif choice == '3' or choice == 'addition':
+#         n1=float(input('Enter your first number for addition'))
+#         n2 = float(input('Enter your second number for addition'))
+#         print(n1+n2)
+#     elif choice == '4' or choice == 'subtraction':
+#         n1=float(input('Enter your first number for subtraction'))
+#         n2 = float(input('Enter your second number for subtraction'))
+#         print(n1 - n2)
+#     elif choice == '5' or choice == 'multiplication':
+#         n1 = float(input('Enter your first number for multiplication'))
+#         n2 = float(input('Enter your second number for multiplication'))
+#         print(n1 * n2)
+#     elif choice == '6' or choice == 'division':
+#         n1 = float(input('Enter your first number for division'))
+#         n2 = float(input('Enter your second number for division'))
+#         if n2 == 0:
+#             print('Zero division error')
+#         else:
+#             print(n1 / n2)
+#     elif choice == '7' or choice == 'exit':
+#         print('Code exited')
+#         break
+#     else:
+#         print('Please enter a choice in the menu............')
+
+#3.Factorial of a number 4,6
+# def fact_of_a_num(num):
+#     fact=1
+#     for i in range (1,num+1):
+#         fact*=i
+#     print(fact)
+# num=int(input('Enter a number to find its factorial'))
+# fact_of_a_num(num)
+
+#4.Print all numbers from 1 to 100 that are divisible by 3 and 5 using a for loop
+# def div_by(num):
+#     for i in range(1,num+1):
+#         if (i % 3 == 0 ) and (i % 5 == 0):
+#             print(i)
+# num = int(input('Enter a number:'))
+# div_by(num)
+
+
+
